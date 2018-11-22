1e3bf2f1b31f4fcb591d98da8a34eb19229a39a8
irishkus <solovieva3011@gmail.com>
2018-11-22 09:39:46 +0300

    Сделаны первые 5 заданий из домашнего задания

---
+++ 2l_SolovievaIrina.playground/2l_SolovievaIrina.playground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2l_SolovievaIrina.playground
+//
+//  Created by Ирина on 21.11.2018.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
