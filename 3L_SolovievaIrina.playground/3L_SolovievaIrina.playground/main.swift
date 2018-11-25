fb3f72f77f7abd5048f676ae76a768175ce9416c
irishkus <solovieva3011@gmail.com>
2018-11-25 20:59:49 +0300

    Описать несколько структур – любой легковой автомобиль и любой грузовик. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна

---
+++ 3L_SolovievaIrina.playground/3L_SolovievaIrina.playground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  3L_SolovievaIrina.playground
+//
+//  Created by Ирина on 25.11.2018.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
