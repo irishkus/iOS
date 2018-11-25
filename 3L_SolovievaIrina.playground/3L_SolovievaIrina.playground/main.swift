c41975599f26f484a5f9dd71a412031114dfa12e
irishkus <solovieva3011@gmail.com>
2018-11-25 21:00:01 +0300

    Описать несколько структур – любой легковой автомобиль и любой грузовик. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна

--- 3L_SolovievaIrina.playground/3L_SolovievaIrina.playground/main.swift
+++ 3L_SolovievaIrina.playground/3L_SolovievaIrina.playground/main.swift
@@ -7,6 +7,52 @@
 //
 
 import Foundation
+// перечисление открыть/закрыть окна
+enum OpenWindows {
+    case open, close
+}
+//перечисление запустить/заглушить двигатель
+enum Engine {
+    case run, kill
+}
+//перечисление моделей
+enum Model {
+    case geely, honda, renault, peugeot, mercedes, kamaz
+}
+//перечисление груза
+//enum Сargo {
+//    case load(kg:Double)
+//    case unload(kg:Double)
+//}
+//структура легковой автомобиль
+struct Car {
+    let year: Int
+    let model: Model
+    let luggageCapacity: Double
+    var luggageCapacityBusy: Double
+    var engine: Engine
+    var openWindows: OpenWindows
+    
+}
+//структура грузовик
 
-print("Hello, World!")
+struct Truck {
+    let year: Int
+    let model: Model
+    let bodyCapacity: Double
+    var bodyCapacityBusy: Double
+    var engine: Engine
+    var openWindows: OpenWindows
+}
+
+let car1 = Car (year: 4, model: .geely, luggageCapacity: 30.0, luggageCapacityBusy: 20.5, engine: .run, openWindows: .close)
+
+let car2 = Car (year: 10, model: .renault, luggageCapacity: 35.0, luggageCapacityBusy: 5, engine: .kill, openWindows: .open)
+
+let truck1 = Truck (year: 1, model: .mercedes, bodyCapacity: 350.0, bodyCapacityBusy: 50, engine: .run, openWindows: .open)
+
+
+
+
+
 
