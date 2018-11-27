6fd79edfd2f0612b2fa6a7e7c8a800c874d7c5f7
irishkus <solovieva3011@gmail.com>
2018-11-27 10:31:31 +0300

    окончательная версия дз 3

--- main.swift
+++ main.swift
@@ -1,134 +1,162 @@
 //
 //  main.swift
-//  2l_SolovievaIrina.playground
+//  3L_SolovievaIrina.playground
 //
-//  Created by Ирина on 21.11.2018.
+//  Created by Ирина on 25.11.2018.
 //  Copyright © 2018 Ирина. All rights reserved.
 //
 
 import Foundation
 
-//функция, которая определяет, четное число или нет
+// перечисление открыть/закрыть окна
+enum WindowsState {
+    case open, close
+}
+//перечисление запустить/заглушить двигатель
+enum EngineState {
+    case run, kill
+}
+//перечисление моделей
+enum Model {
+    case geely, honda, renault, peugeot, mercedes, kamaz
+}
+//печисление изменение свойств
+enum EditProperties {
+    case openWindows
+    case closeWindows
+    case runEngine
+    case killEngine
+}
+//структура легковой автомобиль
+struct Car {
+    let year: Int
+    let model: Model
+    let luggageCapacity: Double
+    //отслеживаем загрузку/выгрузку груза из багажника
+    var luggageCapacityBusy: Double {
+        didSet {
+            let cargo = luggageCapacityBusy - oldValue
+            if cargo > 0 {
+                print ("Загрузили в багажник груз объемом \(cargo)")
+            } else if cargo < 0 {
+                print ("Выгрузили из багажника груз объемом \(abs(cargo))")
+            } else {print ("В багажнике все осталось как было")}
+        }
+    }
+    //отслеживаем запуск/остановку двигателя
+    var engineState: EngineState {
+        willSet {
+            if newValue == .run {
+                print("Двигатель сейчас запустится")
+            } else {
+                print("Двигатель сейчас заглушится")
+            }
+        }
+    }
+    //отслеживаем открытие/закрытие окон
+    var windowsState: WindowsState{
+        willSet {
+            if newValue == .open {
+                print("Окна сейчас откроются")
+            } else {
+                print("Окна сейчас закроются")
+            }
+        
+        }
+    }
+    //метод, который меняет свойства структуры в зависимости от действий
+    mutating func editPropirtiesStuct(edit: EditProperties) {
+        switch edit {
+        case .openWindows:
+            self.windowsState = .open
+        case .closeWindows:
+            self.windowsState = .close
+        case .runEngine:
+            self.engineState = .run
+        case .killEngine:
+            self.engineState = .kill
+        }
+    }
+    func printProperties() {
+        print("Легковой автомобиль года выпуска \(year), марки \(model), c объемом багажника \(luggageCapacity) и занятым объемом \(luggageCapacityBusy), имеет двигатель в состоянии \(engineState) и окна в состоянии \(windowsState)")
+    }
+    
+}
+//структура грузовик
+struct Truck {
+    let year: Int
+    let model: Model
+    let bodyCapacity: Double
+    //отслеживаем загрузку/выгрузку груза из кузова
+    var bodyCapacityBusy: Double {
+        didSet {
+            let cargo = bodyCapacityBusy - oldValue
+            if cargo > 0 {
+                print ("Загрузили в кузов груз объемом \(cargo)")
+            } else if cargo < 0 {
+                print ("Выгрузили из кузова груз объемом \(abs(cargo))")
+            } else {print ("В кузове все осталось как было")}
+        }
+    }
+    //отслеживаем запуск/остановку двигателя
+    var engineState: EngineState {
+        willSet {
+            if newValue == .run {
+                print("Двигатель сейчас запустится")
+            } else {
+                print("Двигатель сейчас заглушится")
+            }
+        }
+    }
+    //отслеживаем открытие/закрытие окон
+    var windowsState: WindowsState {
+        willSet {
+            if newValue == .open {
+                print("Окна сейчас откроются")
+            } else {
+                print("Окна сейчас закроются")
+            }
+        }
+    }
 
-func even (number: Int) -> Bool{
-    let n:Int = number
-    if (n % 2) == 0{
-        return true
-    } else {
-        return false
+    //метод, который меняет свойства структуры в зависимости от действий
+    mutating func editPropirtiesStuct(edit: EditProperties) {
+        switch edit {
+        case .openWindows:
+            self.windowsState = .open
+        case .closeWindows:
+            self.windowsState = .close
+        case .runEngine:
+            self.engineState = .run
+        case .killEngine:
+            self.engineState = .kill
+        }
+    }
+    func printProperties() {
+        print("Грузовик года выпуска \(year), марки \(model), c объемом кузова \(bodyCapacity) и занятым объемом \(bodyCapacityBusy), имеет двигатель в состоянии \(engineState) и окна в состоянии \(windowsState)")
     }
 }
 
-//проверяем работу функции
+// инициализация нескольки экземпляров структур
+var car1 = Car (year: 2013, model: .geely, luggageCapacity: 30.0, luggageCapacityBusy: 20.5, engineState: .run, windowsState: .close)
 
-print("работа функции even")
-print(String(even (number: 3)))
-print(String(even (number: 13)))
-print(String(even (number: 11)))
-print(String(even (number: 12)))
-print(String(even (number: 4)))
+var car2 = Car (year: 2008, model: .renault, luggageCapacity: 35.0, luggageCapacityBusy: 5, engineState: .kill, windowsState: .open)
 
-//функция, которая определяет, делится ли число без остатка на 3
+var truck1 = Truck (year: 2000, model: .mercedes, bodyCapacity: 350.0, bodyCapacityBusy: 50, engineState: .run, windowsState: .open)
 
-func division(number: Int) -> Bool{
-    let n:Int = number
-    if (n % 3) == 0{
-        return true
-    } else {
-        return false
-    }
-}
-
-//проверяем работу функции
-
-print("работа функции division")
-print(String(division (number: 3)))
-print(String(division (number: 13)))
-print(String(division (number: 11)))
-print(String(division (number: 12)))
-print(String(division (number: 4)))
-
-//Создаем возрастающий массив из 100 чисел
-
-var newArray: Array<Int> = []
-for i in (0...99) {
-    newArray.append(i+1)
-}
-
-//Удаляем из массива все четные числа и все числа, которые не делятся на 3
-
-var i:Int = 0
-while i < newArray.count  {
-    if even(number: newArray[i])||(!division (number: newArray[i])){
-        newArray.remove(at: i)
-    }else {i+=1}
-}
-
-//выводим массив после удаления
-
-print("массив после удаления четных чисел и чисел, которые не делятся на 3")
-print(newArray)
-
-// функция, которая добавляет в массив новое число Фибоначчи
-
-func fibonacci (array: inout [Double]) {
-    if (array.count>=2){
-        let i = array.count
-        let n: Double = array[i-1] + array[i-2]
-        array.append(n)
-    }else {
-        print ("Недостаточно элементов в массиве")
-    }
-}
-
-//создаем массив и добавляем при помощи функции Фибоначчи 100 элементов в массив.
-
-var fibonaciArray: Array = [0.0,1.0]
-for _ in (1...100){
-    fibonacci(array: &fibonaciArray)
-}
-
-//выводим массив с числами Фибоначчи
-
-print("Массив с числами Фибоначчи")
-print(fibonaciArray)
-
-//Заполняем массив из 100 элементов различными простыми числами.
-//для этого выписываем подряд все целые числа от двух до n
-
-var simpleNumberArray = [Int]()
-var p: Int = 2 //переменная p изначально равна двум – первому простому числу.
-var n:Int = 1000
-for i in (2...(n)){
-    simpleNumberArray.append(i)
-}
-
-//Функция, которая зачеркивает (заменяет на 0) в списке числа от 2p до n, считая шагами по p
-func crossOut (array: inout [Int], p: Int, n: Int){
-    for a in stride(from: 2*p-2, to: n-1, by: p){
-        array[a] = 0
-    }
-}
-
-//повторяем шаги с зачеркиванием не простых чисел и увеличением р
-while p < n/2 {
-    crossOut(array: &simpleNumberArray, p: p, n: n)
-    i = 0
-    while i<simpleNumberArray.count{
-        if simpleNumberArray[i] > p {
-            p = simpleNumberArray[i] //Находим первое не зачеркнутое число в списке, большее, чем p, и присваиваем значению переменной p это число
-            break
-        }else {i+=1}
-    }
-}
-i = 0
-//оставляем массив из 100 простых чисел
-while i<simpleNumberArray.count{
-    if (simpleNumberArray[i] == 0)||(i>=100) {
-        simpleNumberArray.remove(at: i)
-    } else {i+=1}
-}
-
-//выводим массив с простыми числами из 100 элементов
-print("Массив с простыми числами ")
-print(simpleNumberArray)
+//применяем различные действия к структурам
+car1.engineState = .kill
+car1.windowsState = .open
+car1.luggageCapacityBusy = 20
+car2.engineState = .run
+truck1.windowsState = .close
+truck1.bodyCapacityBusy = 63
+truck1.engineState = .kill
+car1.editPropirtiesStuct(edit: .runEngine)
+truck1.editPropirtiesStuct(edit: .openWindows)
+car2.luggageCapacityBusy = 10
+truck1.bodyCapacityBusy = 85
+//Выводим значения свойств экземпляров в консоль.
+car1.printProperties()
+car2.printProperties()
+truck1.printProperties()
