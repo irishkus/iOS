e21272bf8925c8aab55c14202b8357c8988d33e3
irishkus <solovieva3011@gmail.com>
2018-11-30 14:47:16 +0300

    Домашнее задание 4

---
+++ 4L_SolovievaIrina/4L_SolovievaIrina/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  4L_SolovievaIrina
+//
+//  Created by Ирина on 29.11.2018.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
