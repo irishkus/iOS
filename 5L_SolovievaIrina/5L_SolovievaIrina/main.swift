9ba7bccf551ca5d3964d02c2da4af29368d0595a
irishkus <solovieva3011@gmail.com>
2018-12-05 22:42:32 +0300

    ДЗ 5

---
+++ 5L_SolovievaIrina/5L_SolovievaIrina/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  5L_SolovievaIrina
+//
+//  Created by Ирина on 03.12.2018.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
