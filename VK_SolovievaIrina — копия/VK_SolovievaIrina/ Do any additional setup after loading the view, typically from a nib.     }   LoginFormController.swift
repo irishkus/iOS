b6c8e7b957d82b97a7b2f07fdceefa99a7c0e8e7
irishkus <solovieva3011@gmail.com>
2018-12-18 17:28:19 +0300

    Домашнее задание к лекции 1 курс 2

---
+++ VK_SolovievaIrina — копия/VK_SolovievaIrina/ Do any additional setup after loading the view, typically from a nib.     }   LoginFormController.swift
@@ -0,0 +1,30 @@
+//
+//   Do any additional setup after loading the view, typically from a nib.     }   LoginFormController.swift
+//  VK_SolovievaIrina
+//
+//  Created by Ирина on 16.12.2018.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import UIKit
+
+class _Do_any_additional_setup_after_loading_the_view__typically_from_a_nib__________LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
